ea821583122448d674eddd4b8a4e1f80267741b4
prabhat <prabhatshukla0849@gmal.com>
2024-08-05 21:02:07 +0530

    aws-sdk functionality added

---
+++ src/aws.ts
@@ -0,0 +1,20 @@
+import { S3 } from "aws-sdk";
+import fs from "fs";
+
+const s3 = new S3({
+    accessKeyId: "jx246knid73al2qe2ny4dxq3xy2q",
+    secretAccessKey: "jyxdn5vg4brtuosemik42t277vupagjothjf57d34ftf2n7nl4y4i",
+    endpoint: "https://gateway.storjshare.io"
+})
+
+// fileName => output/12312/src/App.jsx
+// filePath => /Users/harkiratsingh/vercel/dist/output/12312/src/App.jsx
+export const uploadFile = async (fileName: string, localFilePath: string) => {
+    const fileContent = fs.readFileSync(localFilePath);
+    const response = await s3.upload({
+        Body: fileContent,
+        Bucket: "vercel",
+        Key: fileName,
+    }).promise();
+    console.log(response);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+// satellite address :121RTSDpyNZVcEU84Ticf2L1ntiuUimbWgfATz21tuvgk3vzoA6@ap1.storj.io:7777 
+// API  key15XZhFUcvdb4RoK9KSs4E8BBsE9XKfCqxwBCPi7GaMGnaPRq5RF23n7MoFcWeQfcywgF5xvaD44JDbxQf9o5RmzNiogfp31Dtq7zK5GxQn1sgzrH2gd2p98nEVszK4ttXFRq5viEy
+
+
+// s3
+// access key ; jx246knid73al2qe2ny4dxq3xy2q
+// secret key: jyxdn5vg4brtuosemik42t277vupagjothjf57d34ftf2n7nl4y4i
+// endpoint:https://gateway.storjshare.io
+
 import express from "express"
 import cors from  "cors"
 import { generate } from "./utils";
